5a953c105ddc97e3836831eb317fe470d78784a1
oussema-96 <65921823+oussema-96@users.noreply.github.com>
2022-11-14 09:27:27 +0100

    Add files via upload

---
+++ Osmond_SocketServerApi/Controllers/WebSocketController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Osmond_SocketServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebSocketController : ControllerBase
+    {
+        private readonly ILogger<WebSocketController> _logger;
+
+        public WebSocketController(ILogger<WebSocketController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                _logger.Log(LogLevel.Information, "WebSocket connection established");
+                await Echo(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message received from Client");
+
+            while (!result.CloseStatus.HasValue)
+            {
+                /*var serverMsg = Encoding.UTF8.GetBytes($"Server: Hello. You said: {Encoding.UTF8.GetString(buffer)}");
+                await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                _logger.Log(LogLevel.Information, "Message sent to Client");*/
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                _logger.Log(LogLevel.Information, "Message received from Client");
+
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            _logger.Log(LogLevel.Information, "WebSocket connection closed");
+        }
+    }
+}
